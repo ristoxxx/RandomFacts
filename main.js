82306a52fba8c85a73d82c2a825ad4b586727002
ristoxxx <lahteri@gmail.com>
2024-01-17 12:09:55 +0200

    Update main.js

---
+++ main.js
@@ -0,0 +1,48 @@
+const facts = [
+    {
+        id: 1,
+        text: "I am 44 years old.",
+    },
+    {
+        id: 2,
+        text: "I am a web developer.",
+    },
+    {
+        id: 3,
+        text: "I live in Helsinki.",
+    }
+];
+
+const btn = document.getElementById('btn');
+
+function newFact() {
+    const randomFact = Math.floor(Math.random() * facts.length);
+    document.getElementById('text').innerHTML = facts[randomFact].text;
+}
+
+
+
+const currentDate = new Date();
+
+const year = currentDate.getFullYear();
+
+const week = currentDate.getDay();
+
+const month = currentDate.getMonth();
+
+const dateItems = [
+    year, week, month
+];
+function randomDateItem() {
+    const item =[Math.floor(Math.random() * dateItems.length)];
+    document.getElementById('date').innerHTML = dateItems[0].text;
+};
+
+function doAll() {
+    newFact();
+    randomDateItem();
+}
+
+btn.addEventListener('click', doAll);
+
+
